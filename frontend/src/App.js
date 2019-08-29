d7f42f954f32f257e4659d9d2db33fd2b398d938
Luke <lukerbab@gmail.com>
2019-08-29 18:00:12 +0100

    feature: removed unneeded logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
 import AppRouter      from './Router';
-import { Provider }   from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { store }      from './redux/store/store';
 import { initialize } from './services/bootstrap.service';
 
 initialize();
 
 function App() {
 	return (
-		<Provider store={store}>
-			<AppRouter/>
-		</Provider>
+		<AppRouter/>
 	);
 }
 
--- frontend/src/redux/selectors/authenticated.selectors.js
+++
@@ -1,5 +0,0 @@
-export const getAuthenticatedState = store => {
-	return {
-		authenticated: store.authenticated
-	};
-};
